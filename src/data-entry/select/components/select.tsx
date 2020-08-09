3c789093edd66d2dc13c2e11814a9cb982e4598a
yunliang-ding <804408086@qq.com>
2020-08-09 19:36:55 +0800

    bugfix

--- src/data-entry/select/components/select.tsx
+++ src/data-entry/select/components/select.tsx
@@ -1,25 +1,3 @@
-/**
- * 
-| **属性名**           | **类型**                                | **描述**                            | **默认** |
-| -------------------- | --------------------------------------- | ----------------------------------- | -------- |
-| value                | string/string[]                         | 指定当前选中的条目                  | 无       |
-| options              | string[]                                | 下拉选项                           | 无       |
-| placeholder          | string                                  | 提示文案                           | 无       |
-| allowClear           | boolean                                 | 支持清除                            | false    |
-| autoClearSearchValue | boolean                                 | 是否在选中项后清空搜索框            | true     |
-| autoFocus            | boolean                                 | 默认获取焦点                        | false    |
-| disabled             | boolean                                 | 是否禁用                             | false    |
-| dropdownClassName    | object                                  | 下拉菜单的 style 属性               | 无       |
-| dropdownStyle        | object                                  | 下拉菜单的 style 属性               | 无       |
-| filterOption         | boolean or function(inputValue, option) | 过滤                                | true     |
-| getPopupContainer    | Function(dom) () => document.body       | 菜单渲染父节点                      | 无       |
-| multiple             | boolean                                 | 是否支持多选                        | true     |
-| showArrow            | boolean                                 | 是否显示下拉小箭头                  | true     |
-| showSearch           | boolean                                 | 使单选模式可搜索                    | false    |
-| open                 | boolean                                 | 是否展开下拉菜单                    | false    |
-| onChange             | function(value, option)                 | 选中 option，或 input 的 value 变化 | 无       |
-| onSearch             | function(value:string)                  | 文本框值变化时回调                  | 无       |
- */
 import React, { useState } from 'react'
 import { Icon } from '../../../index'
 export default ({
@@ -28,6 +6,7 @@ export default ({
   allowClear = false,
   placeholder,
   disabled = false,
+  style = {},
   dropdownClassName,
   dropdownStyle = {},
   onChange,
@@ -37,7 +16,7 @@ export default ({
   let className = _open ? 'sui-select sui-select-open' : 'sui-select'
   disabled && (className += ' sui-select-disabled')
   const dropDownClassName = dropdownClassName ? dropdownClassName + ' sui-select-dropdown' : 'sui-select-dropdown'
-  const _options = Array.isArray(options) ? options.map(option => {
+  const _options = Array.isArray(options) ? options.map(option => { // 组装options
     return {
       key: Math.random(),
       label: typeof option === 'string' ? option : option.label,
@@ -45,22 +24,22 @@ export default ({
       disabled: typeof option === 'string' ? false : option.disabled
     }
   }) : []
-  const selected: any = _options.find(item => item.value === value) || {}
-  return <div className={className}>
+  const selected: any = _options.find(item => item.value === value) || {} // 选中项
+  return <div className={className} style={style}>
     <div className='sui-select-selection' onClick={
       () => {
         if (disabled) return
         setopen(!_open)
       }
     }>
-      <div className='sui-select-selection-selected'>
+      <div className='sui-select-selection-selected-value'>
         {selected.value === undefined ? <span style={{ color: '#aaa' }}>{placeholder}</span> : selected.label}
       </div>
       <Icon type='iconxialadown' />
       {
         allowClear && selected.value !== undefined && <Icon type='iconcuo' onClick={
           (e) => {
-            e.stopPropagation()
+            e.stopPropagation() // 组织冒泡
             typeof onChange === 'function' && onChange(null, null)
           }
         } />
--- src/data-entry/select/index.tsx
+++ src/data-entry/select/index.tsx
@@ -1,5 +1,28 @@
+/**
+ * 
+| **属性名**           | **类型**                                | **描述**                            | **默认** |
+| -------------------- | --------------------------------------- | ----------------------------------- | -------- |
+| value                | string/string[]                         | 指定当前选中的条目                  | 无       |
+| options              | string[]                                | 下拉选项                           | 无       |
+| placeholder          | string                                  | 提示文案                           | 无       |
+| allowClear           | boolean                                 | 支持清除                            | false    |
+| autoClearSearchValue | boolean                                 | 是否在选中项后清空搜索框            | true     |
+| autoFocus            | boolean                                 | 默认获取焦点                        | false    |
+| disabled             | boolean                                 | 是否禁用                             | false    |
+| style                | object                                  | 输入框 style 属性                    | 无       |
+| dropdownClassName    | object                                  | 下拉菜单的 style 属性               | 无       |
+| dropdownStyle        | object                                  | 下拉菜单的 style 属性               | 无       |
+| filterOption         | boolean or function(inputValue, option) | 过滤                                | true     |
+| getPopupContainer    | Function(dom) () => document.body       | 菜单渲染父节点                      | 无       |
+| multiple             | boolean                                 | 是否支持多选                        | true     |
+| showArrow            | boolean                                 | 是否显示下拉小箭头                  | true     |
+| showSearch           | boolean                                 | 使单选模式可搜索                    | false    |
+| open                 | boolean                                 | 是否展开下拉菜单                    | false    |
+| onChange             | function(value, option)                 | 选中 option，或 input 的 value 变化 | 无       |
+| onSearch             | function(value:string)                  | 文本框值变化时回调                  | 无       |
+ */
 import React from 'react'
 import { Select, SelectMultiple } from './components'
 export default (props:any) => {
-  return props.mode === 'multiple' ? <SelectMultiple {...props} /> : <Select {...props} /> 
+  return props.multiple ? <SelectMultiple {...props} /> : <Select {...props} /> 
 }
