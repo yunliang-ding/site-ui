dbc0ec597ec64c963593a9072b12f14fa14bdd53
yunliang <yunliang@ding.com>
2020-08-13 20:57:24 +0800

    bugfix

---
+++ src/data-display/tooltip/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+/**
+| **属性名**   | **类型**             | **描述**     | **默认**   |
+| ------------ | -------------------- | ------------ | ---------- |
+| size         | number   | 字体大小       | 20       |
+| style        | Object   | 样式          | 无       |
+| type         | string    | icon        | 无       |
+| onClick      | function(e) | 点击回调    | 无       |
+ */
+export default ({ type, size, style, onClick }:any) => {
+  let _style = style || {}
+  _style.fontSize = size
+  return <>
+    <i className={'sui-icon ' + type} style={_style} onClick={
+      (e:any) => {
+        typeof onClick === 'function' && onClick(e)
+      }
+    } />
+  </>
+}
