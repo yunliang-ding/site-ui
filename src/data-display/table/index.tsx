60008968c8617ae3fcace4c9599f84ffa79992a3
yunliang <yunliang@ding.com>
2020-08-21 17:38:14 +0800

    fixed

--- src/data-display/table/index.tsx
+++ src/data-display/table/index.tsx
@@ -43,19 +43,32 @@ export default ({
    */
   const tableHeaderRef = useRef()
   const tableBodyRef = useRef()
-  const tableFooterRef = useRef()
+  const tableBoxRef = useRef()
+  const tableFixedLeftRef = useRef()
+  const tableFixedRightRef = useRef()
   const setWidthSize = () => {
-    tableHeaderRef.current.style.width = tableBodyRef.current.getBoundingClientRect().width + 'px'
+    tableHeaderRef.current && (tableHeaderRef.current.style.width = tableBodyRef.current.getBoundingClientRect().width + 'px')
+    // 宽度不够才展示fixed
+    tableBoxRef.current && setshowFixed(tableBoxRef.current.getBoundingClientRect().width < tableBodyRef.current.getBoundingClientRect().width)
+  }
+  const setFixScrollTop = (scrollTop) => {
+    if(tableFixedLeftRef.current && tableFixedRightRef.current){
+      tableFixedLeftRef.current.scrollTop = scrollTop
+      tableFixedRightRef.current.scrollTop = scrollTop
+    }
   }
   /**
-   * 监听resize事件
+   * 监听事件
    */
   useEffect(() => {
-    window.addEventListener('resize', debounce(() => {
+    window.addEventListener('resize', debounce(() => { // 监听resize事件
       setWidthSize()
     }))
+    tableBodyRef.current.addEventListener('scroll', debounce(() => { // 监听scroll事件
+      setFixScrollTop(tableBodyRef.current.scrollTop)
+    }))
   }, [])
-  useEffect(()=>{
+  useEffect(() => {
     setWidthSize()
   })
   // debounce 防抖
@@ -76,14 +89,17 @@ export default ({
    */
   useEffect(() => {
     if (Array.isArray(columns)) {
-      setcolumns(columns)
+      setcolumns([...columns])
     }
   }, [columns])
   useEffect(() => {
     if (Array.isArray(dataSource)) {
-      setdataSource(dataSource)
+      setdataSource([...dataSource])
     }
   }, [dataSource])
+  /**
+   * 内部多选
+   */
   useEffect(() => {
     if (checkable) {
       let column = {
@@ -120,10 +136,15 @@ export default ({
       } else {
         _columns[0] = column
       }
-      console.log('_cocheckablelumns', checkable)
       setcolumns([..._columns])
     }
   }, [checkable, checkedkeys])
+  /**
+   * 内部排序
+   * @param sort 
+   * @param orderBy 
+   * @param column 
+   */
   const sort = (sort, orderBy, column) => {
     if (typeof sort === 'function') {
     } else {
@@ -137,79 +158,123 @@ export default ({
       setdataSource([..._dataSource]) // render
     }
   }
-  return <>
-    <div className='sui-table-wrapper' style={style}>
-      <div className='sui-table-header' ref={tableHeaderRef}>
-        <div className='sui-table'>
-          <div className='sui-table-tr'>
+  
+  /**
+   * 渲染表头
+   * @param columns 
+   */
+  const renderHeaderTable = (columns) => {
+    return <div className='sui-table'>
+      <div className='sui-table-tr'>
+        {
+          columns.map(column => {
+            let minWidth = column.width || (100 / columns.length + '%')
+            let columnClassName = ['sui-table-td']
+            if (column.sort) {
+              columnClassName.push('sui-table-td-sort')
+            }
+            if (bordered) {
+              columnClassName.push('sui-table-td-grid')
+            }
+            return (
+              <div className={columnClassName.join(' ')} key={column.dataIndex} style={{ minWidth, width: minWidth }}>
+                {column.title}
+                {
+                  column.sort && <>
+                    <Icon type='iconxiala1' size={12} style={{ left: 4, top: -6 }} onClick={
+                      () => {
+                        sort(column.sort, 'asc', column.dataIndex)
+                      }
+                    } />
+                    <Icon type='iconxialadown' size={12} style={{ top: 6, right: 8 }} onClick={
+                      () => {
+                        sort(column.sort, 'desc', column.dataIndex)
+                      }
+                    } />
+                  </>
+                }
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  }
+  /**
+   * 渲染主体表格
+   * @param dataSource 
+   * @param columns 
+   */
+  const renderBodyTable = (dataSource, columns) => {
+    return <div className='sui-table'>
+      {
+        dataSource.map((data, index) => {
+          return <div key={data[rowKey]} className='sui-table-tr'>
             {
-              _columns.map(column => {
-                let minWidth = column.width || (100 / _columns.length + '%')
+              columns.map(column => {
+                let minWidth = column.width || (100 / columns.length + '%')
+                let label = column.render ? column.render(data[column.dataIndex], data, index) : data[column.dataIndex]
                 let columnClassName = ['sui-table-td']
-                if (column.sort) {
-                  columnClassName.push('sui-table-td-sort')
+                if (column.ellipsis) {
+                  columnClassName.push('sui-table-td-ellipsis')
                 }
                 if (bordered) {
                   columnClassName.push('sui-table-td-grid')
                 }
-                return (
-                  <div className={columnClassName.join(' ')} key={column.dataIndex} style={{ minWidth, width: minWidth}}>
-                    {column.title}
-                    {
-                      column.sort && <>
-                        <Icon type='iconxiala1' size={12} style={{ left: 4, top: -6 }} onClick={
-                          () => {
-                            sort(column.sort, 'asc', column.dataIndex)
-                          }
-                        } />
-                        <Icon type='iconxialadown' size={12} style={{ top: 6, right: 8 }} onClick={
-                          () => {
-                            sort(column.sort, 'desc', column.dataIndex)
-                          }
-                        } />
-                      </>
-                    }
-                  </div>
-                )
+                return <div title={typeof label !== 'object' && label} key={column.dataIndex} className={columnClassName.join(' ')} style={{ minWidth, width: minWidth }}>{label}</div>
               })
             }
           </div>
+        })
+      }
+    </div>
+  }
+  /**
+   * 左右 fixed 列
+   */
+  const fixedLeft = _columns.filter(item => item.fixed === 'left' || item.dataIndex === 'sui-checked-930226') // 内部全选
+  const fixedRight = _columns.filter(item => item.fixed === 'right')
+  const [showFixed, setshowFixed] = useState(false)
+  return <>
+    <div className='sui-table-wrapper' style={style}>
+      <div className='sui-table-box' ref={tableBoxRef}>
+        <div className='sui-table-header' ref={tableHeaderRef}>
+          {renderHeaderTable(_columns)}
+        </div>
+        <div className='sui-table-body' ref={tableBodyRef}>
+          {renderBodyTable(__dataSource, _columns)}
         </div>
       </div>
-      <div className='sui-table-body' ref={tableBodyRef}>
-        <div className='sui-table'>
-          {
-            __dataSource.map((data, index) => {
-              return <div key={data[rowKey]} className='sui-table-tr'>
-                {
-                  _columns.map(column => {
-                    let minWidth = column.width || (100 / _columns.length + '%')
-                    let label = column.render ? column.render(data[column.dataIndex], data, index) : data[column.dataIndex]
-                    let columnClassName = ['sui-table-td']
-                    if (column.ellipsis) {
-                      columnClassName.push('sui-table-td-ellipsis')
-                    }
-                    if (bordered) {
-                      columnClassName.push('sui-table-td-grid')
-                    }
-                    return <div title={typeof label !== 'object' && label} key={column.dataIndex} className={columnClassName.join(' ')} style={{ minWidth, width: minWidth }}>{label}</div>
-                  })
-                }
-              </div>
-            })
-          }
+      {
+        showFixed && fixedLeft.length > 0 && <div className='sui-table-fixed sui-table-fixed-left'>
+          <div className='sui-table-header'>
+            {renderHeaderTable(fixedLeft)}
+          </div>
+          <div className='sui-table-body' ref={tableFixedLeftRef}>
+            {renderBodyTable(__dataSource, fixedLeft)}
+          </div>
         </div>
-      </div>
+      }
+      {
+        showFixed && fixedRight.length > 0 && <div className='sui-table-fixed sui-table-fixed-right'>
+          <div className='sui-table-header'>
+            {renderHeaderTable(fixedRight)}
+          </div>
+          <div className='sui-table-body' ref={tableFixedRightRef}>
+            {renderBodyTable(__dataSource, fixedRight)}
+          </div>
+        </div>
+      }
     </div>
     {
-      typeof pagination === 'object' ? <div className='sui-table-footer' ref={tableFooterRef}>
+      typeof pagination === 'object' ? <div className='sui-table-footer'>
         <Pagination {...pagination} />
-      </div> : pagination !== false && <div className='sui-table-footer' ref={tableFooterRef}>
+      </div> : pagination !== false && <div className='sui-table-footer'>
         <Pagination
           {..._pagination}
           onChange={e => {
             _pagination.current = e
-            setpagination({..._pagination})
+            setpagination({ ..._pagination })
           }}
         />
       </div>
