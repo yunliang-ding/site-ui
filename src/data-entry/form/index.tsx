1d74fe71c13f85b8b7a18de129f63dd5d2924fa8
yunliang <yunliang@ding.com>
2020-08-29 15:52:54 +0800

    bugfix

--- src/data-entry/form/index.tsx
+++ src/data-entry/form/index.tsx
@@ -14,40 +14,158 @@ import {
 export default ({
   fields,
   btns = [],
-  onEvent,
+  onValueChanges,
+  onBtnClick,
   formStyle
 }) => {
   // 按钮事件统一处理
   let forms = Array.isArray(fields) ? fields : []
+  let _btn = Array.isArray(btns) ? btns : []
   forms.forEach(item => item.key = Math.random()) // build key
-  const [_fields, setfields] = useState(forms)
-  const toolBarClick = (type: string) => {
+  _btn.forEach(item => item.key = Math.random()) // build key
+  const [_fields, setfields] = useState(JSON.parse(JSON.stringify(forms))) // deep
+  const btnClick = (type: string) => {
+    // 提交类型需要先做表单验证
     if (type === 'submit') {
-      // 提交类型需要先做验证
-      onEvent(type)
-    } else {
-      onEvent(type)
+      let { errors, values } = validateForm()
+      typeof onBtnClick === 'function' && onBtnClick(type, errors, values)
     }
+  }
+  /**
+   * 校验单个字段
+   */
+  const validateField = (item) => {
+    let error, value;
+    if (item.field.rules && Object.prototype.toString.call(item.field.rules) === '[object Array]') {
+      item.field.rules.some(rule => {
+        if (rule.required === true) { // 校验必填
+          if (
+            item.props.value === '' || 
+            item.props.value === undefined || 
+            item.props.value === null ||
+            (Object.prototype.toString.call(item.props.value) === '[object Array]' && item.props.value.length === 0)
+          ) {
+            error = { field: item.name, message: rule.message }
+            item.field.validateStatus = 'error'
+            item.field.message = rule.message
+          } else {
+            item.field.validateStatus = null
+            item.field.message = null
+            value = item.props.value
+          }
+        } else if (rule.pattern) { // 校验正则
+          if (!new RegExp(rule.pattern).test(item.props.value)) {
+            error = { field: item.name, message: rule.message }
+            item.field.validateStatus = 'error'
+            item.field.message = rule.message
+          } else {
+            item.field.validateStatus = null
+            item.field.message = null
+            value = item.props.value
+          }
+        }
+      })
+    }
+    return {
+      error, value
+    }
+  }
+  /**
+  * 校验整个表单
+  */
+  const validateForm = () => {
+    let errors = [] // 封装错误信息
+    let values = {}
+    _fields.map(field => {
+      let { error, value } = validateField(field)
+      if (error !== undefined) {
+        errors.push(error)
+      }
+      if (value !== undefined) {
+        values[field.name] = value
+      }
+    })
+    if (errors.length > 0) {
+      setfields([..._fields]) // 提示信息
+    }
+    return {
+      errors,
+      values
+    }
+  }
+  /**
+   * field update
+   * @param name 
+   * @param e 
+   */
+  const eventTouch = (name, e) => {
+    // 1:先按照name查找到props的fields,执行field自带的onChange
+    let field = forms.find(form => form.name === name)
+    if (field && field.props.onChange) {
+      field.props.onChange(e)
+    }
+    // 2:内部自动执行双向绑定的自定义事件,赋值
+    let _field = _fields.find(field => field.name === name)
+    if (_field) {
+      _field.props.value = (e && e.target) ? e.target.value : e
+      // 3:开始校验
+      validateField(_field)
+      setfields([..._fields]) // render
+    }
+    // 4:调用外层 onValueChanges
+    typeof onValueChanges === 'function' && onValueChanges(name, e)
   }
   const resolveComponents = (item: any) => {
     if (item.type === 'Input') {
-      return <Input {...item.props} />
+      return <Input {...item.props} onChange={
+        (e) => {
+          eventTouch(item.name, e)
+        }
+      } />
     } else if (item.type === 'Checkbox') {
-      return <Checkbox {...item.props} />
+      return <Checkbox {...item.props} onChange={
+        (e) => {
+          eventTouch(item.name, e)
+        }
+      } />
     } else if (item.type === 'CheckboxGroup') {
-      return <CheckboxGroup {...item.props} />
+      return <CheckboxGroup {...item.props} onChange={
+        (e) => {
+          eventTouch(item.name, e)
+        }
+      } />
     } else if (item.type === 'Select') {
       return (
-        <Select {...item.props} />
+        <Select {...item.props} onChange={
+          (e) => {
+            eventTouch(item.name, e)
+          }
+        } />
       )
     } else if (item.type === 'DatePicker') {
-      return <DatePicker {...item.props} />
+      return <DatePicker {...item.props} onChange={
+        (e) => {
+          eventTouch(item.name, e)
+        }
+      } />
     } else if (item.type === 'Radio') {
-      return <Radio {...item.props} />
+      return <Radio {...item.props} onChange={
+        (e) => {
+          eventTouch(item.name, e)
+        }
+      } />
     } else if (item.type === 'RadioGroup') {
-      return <RadioGroup {...item.props} />
+      return <RadioGroup {...item.props} onChange={
+        (e) => {
+          eventTouch(item.name, e)
+        }
+      } />
     } else if (item.type === 'TimePicker') {
-      return <TimePicker {...item.props} />
+      return <TimePicker {...item.props} onChange={
+        (e) => {
+          eventTouch(item.name, e)
+        }
+      } />
     }
   }
   return <>
@@ -62,13 +180,13 @@ export default ({
         }
       </div>
       {
-        btns.length > 0 && <div className='sui-form-body-btns'>
+        _btn.length > 0 && <div className='sui-form-body-btns'>
           {
-            btns.map((btn: any) => {
+            _btn.map((btn: any) => {
               return <Button
                 key={btn.key}
                 {...btn.props}
-                onClick={toolBarClick.bind(null, btn.type)}
+                onClick={btnClick.bind(null, btn.type)}
               >
                 {btn.label}
               </Button>
